2a3b4f3b6562a5f0af6a9e7891773b73e4b463ce
nazguul <youneshabbal1@gmail.com>
2026-07-23 19:31:51 +0100

    testing out ratatui tutorials

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ pub mod error;
 mod ui;
 
 fn main() -> Result<(), ErrorType> {
+    // this helps us display error in nice formatings
     color_eyre::install().map_err(ErrorType::ColorEyreInstallationFailed)?;
+    // main entry point , where we call our run function to run event loop
     ratatui::run(app).map_err(ErrorType::IOError)?;
     Ok(())
 }
+
+// this is the app event loop
+// we draw things on it
+// we tell the backend if any key pressed return true
+// if true, the even loop breaks, the program ends
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     loop {
         terminal.draw(render)?;
@@ -18,6 +25,9 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
         }
     }
 }
+
+// this is the drawing functin
+// we simple use primitve, which is &str, and define where to put that string. which is the frame area . no layouts , no complex shit
 fn render(frame: &mut Frame) {
     frame.render_widget("hello world", frame.area())
 }
